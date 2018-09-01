e09b0a71285593d62ade01d9207fb07e4ecd5dc9
“yang” <lovepython@foxmail.com>
2018-09-01 10:27:53 +0800

    完成首页的滑窗的效果

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/image/huawei_home.jpg',
+      '/image/huawei_home1.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  onLoad: function () {
+    
+  }
+})
